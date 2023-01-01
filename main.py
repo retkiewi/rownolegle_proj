7b9e617d838431f31333a3c84d64f285405d9638
retkiewi <krzret@student.agh.edu.pl>
2023-01-01 23:02:04 +0100

    zad 2 finished

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 from star import Star
 from mpi4py import MPI
-from math import ceil
+from math import ceil, log10
 
 
 ITERS = 40000
@@ -27,64 +27,59 @@ stars = comm.bcast(stars, root=0)
 
 block_size = ceil(N/size)
 range_start = rank*block_size
-range_end = max((rank+1)*block_size, N)
+range_end = min((rank+1)*block_size, N)
 
-stars = stars[range_start, range_end]
+stars = stars[range_start:range_end]
 
 result = []
 
-print(len(stars), rank)
-
+# print(len(stars), rank)
+padding = int(log10(ITERS))
 for i in range(ITERS):
+    if rank == 0:
+        progress = '-'*(50*i//ITERS)+'>'
+        print(f'ITER {i:>5} [{progress:<50}]',  end='\r')
     # get the buffoer from prev
-    attractions = np.zeros(len(stars))
+    attractions = np.zeros((len(stars),3))
     comm.send(stars, dest=(rank+1)%size, tag=1)
+    comm.send(np.zeros((len(stars),3)), dest=(rank+1)%size, tag=3)
 
-    for j in range(size-1):
-        prev_stars = comm.recv(source=(rank-1)%size, tag=1)
+    for j in range(size//2):
+        buffer_stars = comm.recv(source=(rank-1)%size, tag=1)
+        buffer_attractions = comm.recv(source=(rank-1)%size, tag=3)
 
-        attractions = attractions + [star.calculate_attraction(prev_stars) for star in stars]
-        # other_attractions = [star.calculate_attraction(stars) for star in prev_stars] + other_attractions
+        attractions = attractions + [star.calculate_attraction(buffer_stars) for star in stars]
 
-        if j<size-2:
-            comm.send(prev_stars, dest=(rank+1)%size, tag=1)
+        #avoid calculating attractions twice when we an even number of threads 
+        if size%2 != 0:
+            buffer_attractions =  buffer_attractions + [star.calculate_attraction(stars) for star in buffer_stars]
+
+        if j<size//2-1:
+            comm.send(buffer_stars, dest=(rank+1)%size, tag=1)
+            comm.send(buffer_attractions, dest=(rank+1)%size, tag=3)
+
+    # exchange the attraction accumulator
+    comm.send(buffer_attractions, dest=(rank-size//2)%size, tag=3)
+    buffer_attractions = comm.recv(source=(rank+size//2)%size, tag=3)
+
+    attractions = attractions + buffer_attractions
+
+    if i%print_n==0:
+        if rank == 0:
+            curr_result = [(star.position, int(star.mass**(1/3))) for star in stars]
+            for j in range(1, size):
+                curr_result = curr_result + [(star.position, int(star.mass**(1/3))) for star in comm.recv(source=j, tag=2)]
+            result.append(curr_result)
+        else:
+            comm.send(stars, dest=0, tag=2)
     
-    if rank == 0 and i%print_n==0:
-        curr_result = [*stars]
-        for j in range(1, size):
-            curr_result = curr_result + comm.recv(source=j, tag=2)
-        result.append(curr_result)
-    else:
-        comm.send(stars, dest=0, tag=2)
-    
-
     for star, attraction in zip(stars, attractions):
         star.tick_velocity(attraction)
         star.tick_position()
-    
-
-# for i in range(ITERS):
-#     for star in stars[range_start:range_end]:
-#         star.tick_velocity(stars)
-#     for star in stars[range_start:range_end]:
-#         star.tick_position()
-#     comm.send(stars, dest=(rank+1)%size, tag=1)
-#     comm.send(stars, dest=(rank-2)%size, tag=2)
-#     prev_stars = comm.recv(source=(rank-1)%size, tag=1)
-#     next_stars = comm.recv(source=(rank+2)%size, tag=2)
-#     stars[:range_start] = prev_stars[:range_start]
-#     stars[range_end:] = prev_stars[range_end:]
-#     next_block_end = (range_end+block_size)%N
-#     if next_block_end == 0: next_block_end = 48
-#     stars[range_end%N:next_block_end] = next_stars[range_end%N:next_block_end]
-#     next_block_end = (range_end+block_size*2)%N
-#     if next_block_end == 0: next_block_end = 48
-#     stars[(range_end+block_size)%N:(range_end+block_size*2)%N] = next_stars[(range_end+block_size)%N:(range_end+block_size*2)%N]
-#     if rank == 0 and i%print_n==0:
-#         result.append([(star.position, int(star.mass**(1/3))) for star in stars])
 
 if rank != 0: exit(0)
 
+print()
 
 def update_points(t):
     plt.gca().cla()
